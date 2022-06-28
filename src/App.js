e8d0ca28c9ad60a5ac540152ca06d54fd80e0002
Alexey-Buglak <alexbuglak2007@gmail.com>
2022-06-28 23:47:11 +0200

    Add Hud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// STYLES //
+import './styles/style.scss';
+// COMPONENTS //
+import React from 'react';
+import Hud from './components/Hud';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return <Hud />;
+};
 
 export default App;
